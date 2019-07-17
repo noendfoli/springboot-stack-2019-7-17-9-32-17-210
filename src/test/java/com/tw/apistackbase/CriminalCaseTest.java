3a658ba2f714635429a90e6d27fdec17a250a66f
owen <649073762@qq.com>
2019-07-17 19:33:39 +0800

    story 1 ac1 Add test passed

---
+++ src/test/java/com/tw/apistackbase/CriminalCaseTest.java
@@ -0,0 +1,45 @@
+package com.tw.apistackbase;
+
+import com.tw.apistackbase.entity.CriminalCase;
+import com.tw.apistackbase.repository.CriminalCaseRepository;
+import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+@RunWith(SpringRunner.class)
+@DataJpaTest
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+public class CriminalCaseTest {
+    @Autowired
+    private CriminalCaseRepository criminalCaseRepository;
+
+    @Test
+    public void should_return_size_when_the_add() {
+        //given
+        CriminalCase criminalCase1 = new CriminalCase("xxCase",System.currentTimeMillis());
+        criminalCaseRepository.save(criminalCase1);
+        CriminalCase criminalCase2= new CriminalCase("yyCase",System.currentTimeMillis());
+        criminalCaseRepository.save(criminalCase2);
+        CriminalCase criminalCase3 = new CriminalCase("zzCase",System.currentTimeMillis());
+        criminalCaseRepository.save(criminalCase3);
+        CriminalCase criminalCase4= new CriminalCase("mmCase",System.currentTimeMillis());
+        criminalCaseRepository.save(criminalCase4);
+
+        //when
+        List<CriminalCase> caseList = criminalCaseRepository.findAll();
+        // /then
+        Assertions.assertEquals(4,caseList.size());
+    }
+
+
+}
