6367f4c11d0a1c0ef78c1e1e4f80e82bf0bc0d09
owen <649073762@qq.com>
2019-07-17 21:44:41 +0800

    story when name is null or length greater than 50 throws exception test passed

--- src/test/java/com/tw/apistackbase/ProcuratorateTest.java
+++ src/test/java/com/tw/apistackbase/ProcuratorateTest.java
@@ -13,6 +13,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.LinkedList;
 import java.util.List;
 
 @SpringBootTest
@@ -24,12 +25,38 @@ public class ProcuratorateTest {
     private ProcuratorateRepository procuratorateRepository;
 
     @Test
-    public void should_return_exception_when_name_null() {
+    public void should_throws_exception_when_name_null() {
         //given
         Procuratorate procuratorate = new Procuratorate();
         //when// /then
         Assertions.assertThrows(Exception.class,()->{
            procuratorateRepository.saveAndFlush(procuratorate);
+        });
+
+    }
+    @Test
+    public void should_throws_exception_when_name_gerate_than_50() {
+        //given
+        String name = "珠海检察院"+"sajdlsajdfljsdljflsdajf"+"sajdlsajdfljsdljflsdajf"+
+                "sajdlsajdfljsdljflsdajf"+"sajdlsajdfljsdljflsdajf";
+        Procuratorate procuratorate = new Procuratorate(name);
+        //when// /then
+        Assertions.assertThrows(Exception.class,()->{
+            procuratorateRepository.saveAndFlush(procuratorate);
+        });
+
+    }
+
+    @Test
+    public void should_throws_exception_when_two__same_name() {
+        //given
+        String name = "珠海检察院";
+        Procuratorate procuratorate1 = new Procuratorate(name);
+        Procuratorate procuratorate2 = new Procuratorate(name);
+        procuratorateRepository.saveAndFlush(procuratorate1);
+        //when// /then
+        Assertions.assertThrows(Exception.class,()->{
+            procuratorateRepository.saveAndFlush(procuratorate2);
         });
 
     }
